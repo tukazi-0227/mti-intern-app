1d0c9d033f8a0c30935e4bd5ab67803876d4a473
Ryosuke Saiki <ryosuke3191@gmail.com>
2024-08-08 03:37:54 +0000

    user-updateルートを追加

---
+++ lambda/team2-user-update.js
@@ -0,0 +1,43 @@
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const { marshall } = require("@aws-sdk/util-dynamodb");
+const client = new DynamoDBClient({ region: "ap-northeast-1" });
+const TableName = "team2_User";
+
+exports.handler = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({ message: "" }),
+  };
+
+  // TODO: リクエストボディの中身をJavaScriptオブジェクトに変換し、1つ、あるいは複数の変数に代入する
+  const request_body = JSON.parse(event.body);
+
+  // TODO: paramに更新対象のテーブル名と更新内容を記述
+  const param = {
+    TableName: TableName,
+    Item: marshall(request_body)
+  };
+
+  const command = new PutItemCommand(param);
+
+  try {
+    await client.send(command);
+    // TODO: 更新に成功した場合の処理を記述(response bodyを設定する)
+    response.body = JSON.stringify({
+      userId: request_body.userId,
+    });
+    
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "予期せぬエラーが発生しました。",
+      errorDetail: e.toString(),
+    });
+  }
+
+  return response;
+};
